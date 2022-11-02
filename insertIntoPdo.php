b1dab0f0ef91c91a6edf1fe6af5a9ef2ab8e109f
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-02 13:04:14 -0300

    ok

--- insertIntoPdo.php
+++ insertIntoPdo.php
@@ -20,7 +20,7 @@
         $password = "buystuffpwd";
         $sql = "mysql:host=$servername;dbname=$database;";
         $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-        // Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
+        // Create a new connection to the MySQL database using PDO, $dsn is an object
         try { 
           $dsn = new PDO($sql, $username, $password, $dsn_Options);
           echo "Connected Successfully";
