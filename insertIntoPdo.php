fdacf37eeb5304f462b592850ce6dd97952c54c2
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-02 11:01:50 -0300

    ok

--- insertIntoPdo.php
+++ insertIntoPdo.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 // Here we create a variable that calls the prepare() method of the database object
 // The SQL query you want to run is entered as the parameter, and placeholders are written like this :placeholder_name
-$my_Insert_Statement = $my_Db_Connection->prepare("INSERT INTO Students (name, lastname, email) VALUES (:first_name, :last_name, :email)");
+$my_Insert_Statement = $my_Db_Connection->prepare("INSERT INTO Students (nome, lastname, email) VALUES (:first_name, :last_name, :email)");
 // Now we tell the script which variable each placeholder actually refers to using the bindParam() method
 // First parameter is the placeholder in the statement above - the second parameter is a variable that it should refer to
 $my_Insert_Statement->bindParam(':first_name', $first_Name);
@@ -37,7 +37,7 @@ $my_Insert_Statement->bindParam(':email', $email);
 // Execute the query using the data we just defined
 // The execute() method returns TRUE if it is successful and FALSE if it is not, allowing you to write your own messages here
 if ($my_Insert_Statement->execute()) {
-  echo "New record created successfully";
+  echo "<h3>New record created successfully</h3>";
 } else {
   echo "Unable to create record";
 }
