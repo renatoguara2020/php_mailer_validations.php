41eb8e7743192e0fcdebcbe8c7d7f269d923c5e6
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-04 20:55:15 -0300

    ok

--- examplePDO.php
+++ examplePDO.php
@@ -14,10 +14,10 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->beginTransaction();
 
   // prepare sql and bind parameters
-  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES (:firstname, :lastname, :email)");
+  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
   $stmt->bindParam(':firstname', $firstname);
   $stmt->bindParam(':lastname', $lastname);
   $stmt->bindParam(':email', $email);
