5300e397d2c8be20ed68f8827ff1c68a4421d970
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-06 15:16:20 -0300

    ok

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define("BD_SERVIDOR","localhost");
 define("BD_USUARIO","sistematarefa");
 define("BD_SENHA","root");
-define("BD_BANCO","");
+define("BD_BANCO","celke");
 define("BD_PORT",'3306');
 
 
