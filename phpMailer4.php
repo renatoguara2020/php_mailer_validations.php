ef8a642f4e5deec5a7dda73d84e0f9c8e0a491b0
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-06 14:51:08 -0300

    ok

---
+++ phpMailer4.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('class.phpmailer.php');
+
+$mailer = new PHPMailer();
+$mailer->IsSMTP();
+$mailer->SMTPDebug = 1;
+$mailer->Port = 587; //Indica a porta de conexão 
+$mailer->Host = 'smtplw.com.br';//Endereço do Host do SMTP 
+$mailer->SMTPAuth = true; //define se haverá ou não autenticação 
+$mailer->Username = 'smtplocaweb'; //Login de autenticação do SMTP
+$mailer->Password = 'Gwb9etA323'; //Senha de autenticação do SMTP
+$mailer->FromName = 'Bart S. Locaweb'; //Nome que será exibido
+$mailer->From = 'remetente@email.com.br'; //Obrigatório ser 
+// a mesma caixa postal configurada no remetente do SMTP
+$mailer->AddAddress('destinatario@email.com','Nome do 
+destinatário');
+//Destinatários
+$mailer->Subject = 'Teste enviado através do PHP Mailer 
+SMTPLW';
+$mailer->Body = 'Este é um teste realizado com o PHP Mailer 
+SMTPLW';
+if(!$mailer->Send())
+{
+echo "Message was not sent";
+echo "Mailer Error: " . $mailer->ErrorInfo; exit; }
+print "E-mail enviado!"
+?>
