08bead54a315791931c2306595aba8679f43a402
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-06 16:59:51 -0300

    OK

--- connectionPDO1.php
+++ connectionPDO1.php
@@ -4,7 +4,6 @@
         $username = "u266072517_user";
         $password = "buystuffpwd";
         $sql = "mysql:host=$servername;dbname=$database;";
-        $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
         $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dsn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         // Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
