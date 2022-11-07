057950c15cb719160476aa9ef58f5bdea5aa7b63
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-06 22:19:46 -0300

    ok

--- connectionPDO.php
+++ connectionPDO.php
@@ -2,12 +2,12 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    if (isset($_POST['emailAddress']) && !empty($_POST['emailAddress'])) {
-        $emailAddress = filter_input(INPUT_POST,'emailAddress',FILTER_SANITIZE_EMAIL);
+    if (isset($_POST['nome']) && !empty($_POST['nome'])) {
+        $nome = filter_input(INPUT_POST,'nome',FILTER_SANITIZE_EMAIL);
     }
 
-    if(isset($_POST['password']) && !empty($_POST['password']) && $_POST['password']) {
-        $password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_SPECIAL_CHARS);
+    if(isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_SPECIAL_CHARS);
     }
    
 }
@@ -17,7 +17,7 @@ try {
     $conn = new PDO('mysql:host=localhost;dbname=celke', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare('INSERT INTO (nome, email)VALUES(:nome, :email)');
+    $stmt = $conn->prepare('INSERT INTO (nome, email) VALUES (:nome, :email)');
     $stmt->bindValue(1, ':nome', $nome, PDO::PARAM_STR);
     $stmt->bindValue(2, ':email', $email, PDO::PARAM_STR);
     $stmt->execute();
