4aadba51f78ba85d08f575be25040bca7760492c
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-06 22:46:37 -0300

    ok

---
+++ celkePHPOK.php
@@ -0,0 +1,71 @@
+<?php
+include_once "connectionPDO.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Celke - Formulario com INSERT</title>
+    <link rel="shortcut icon" href="images/favicon.ico" />
+</head>
+
+<body>
+    <h2>Cadastrar Usuário</h2>
+    <?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT); 
+    
+    if(!empty($dados['SendCadUsuario']) && isset($dados['SendCadUsuario']) == true){
+        //var_dump($dados);
+
+        $query_usuario = "INSERT INTO usuarios (nome, email, senha, sists_usuario_id, niveis_acesso_id, created) 
+                VALUES (:nome, :email, :senha, :sists_usuario_id, :niveis_acesso_id, NOW())";
+                $cad_usuario = $conn->prepare($query_usuario);
+                $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
+                $cad_usuario->bindParam(':email', $dados['email'], PDO::PARAM_STR);
+                $senha_cript = password_hash($dados['senha'], PASSWORD_DEFAULT);
+                $cad_usuario->bindParam(':senha', $senha_cript);
+                $cad_usuario->bindParam(':sists_usuario_id', $dados['sists_usuario_id'], PDO::PARAM_INT);
+                $cad_usuario->bindParam(':niveis_acesso_id', $dados['niveis_acesso_id'], PDO::PARAM_INT);
+                $conn->beginTransaction();
+
+                $cad_usuario->execute();
+
+                if($cad_usuario->rowCount() > 0){
+                    echo '<div class="alert alert-success" role="alert">
+                             <h3>New record created Successfully !!!<h3>
+                          </div>';
+                }else{
+                    
+                    echo '<div class="alert alert-danger" role="alert">
+                               Unable to create new record! Try again later !!
+                          </div>';
+                }
+    }
+}
+
+    ?>
+    <form method="POST" action="">
+        <label>Nome: </label>
+        <input type="text" name="nome" placeholder="Nome completo" required /><br><br>
+
+        <label>E-mail: </label>
+        <input type="email" name="email" placeholder="Melhor e-mail do usuário" required /><br><br>
+
+        <label>Senha: </label>
+        <input type="password" name="senha" placeholder="Senha do usuário" required /><br><br>
+
+        <label>Situação do Usuário: </label>
+        <input type="number" name="sists_usuario_id" placeholder="Situação do usuário" required /><br><br>
+
+        <label>Nível de Acesso: </label>
+        <input type="number" name="niveis_acesso_id" placeholder="Nível de acesso do usuário" required /><br><br>
+
+        <input type="submit" value="Cadastrar" name="SendCadUsuario" />
+    </form>
+</body>
+
+</html>
--- connectionPDO.php
+++ connectionPDO.php
@@ -1,27 +1,13 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    if (isset($_POST['nome']) && !empty($_POST['nome'])) {
-        $nome = filter_input(INPUT_POST,'nome',FILTER_SANITIZE_EMAIL);
-    }
-
-    if(isset($_POST['email']) && !empty($_POST['email'])) {
-        $email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-
-    if(isset($_POST['message']) && !empty($_POST['message'])) {
-        $message = filter_input(INPUT_POST,'message',FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-    var_dump($_POST);
-}
+ 
 
 
 try{
     $conn = new PDO('mysql:host=localhost;dbname=celke', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare('INSERT INTO (nome, email) VALUES (:nome, :email, :message11)');
+    $stmt = $conn->prepare('INSERT INTO students (firstname, lastname, datanascimento) VALUES (:nome, :email, :message11)');
     $stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
     $stmt->bindValue(':email', $email, PDO::PARAM_STR);
     $stmt->bindValue(':message1',$message, PDO::PARAM_STR);
@@ -33,25 +19,3 @@ try{
 }
 
  ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form method="POST" action="">
-        <label for="name">Name: <input type="text" name="nome" id="name"></label><br><br>
-        <label for="email">Email: <input type="email" name="email" id="email"></label><br><br>
-        <label for="message">Message: <textarea name="message" id="message" rows="8"
-                cols="20"></textarea></label><br><br>
-        <input type="submit" value="Send">
-    </form>
-</body>
-
-</html>
