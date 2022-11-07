131cf2bd93a4145848052b2bdee34079d1c338b9
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-06 22:14:29 -0300

    ok

--- connectionPDO.php
+++ connectionPDO.php
@@ -1,10 +1,18 @@
 <?php
-/*
- * Melhor prática usando Prepared Statements
- *
- */
 
-$id = 5;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (isset($_POST['emailAddress']) && !empty($_POST['emailAddress'])) {
+        $emailAddress = filter_input(INPUT_POST,'emailAddress',FILTER_SANITIZE_EMAIL);
+    }
+
+    if(isset($_POST['password']) && !empty($_POST['password']) && $_POST['password']) {
+        $password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+   
+}
+
+
 try {
     $conn = new PDO('mysql:host=localhost;dbname=celke', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
