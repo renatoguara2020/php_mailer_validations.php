62c577911cdbb307facaa72f9b160cd0ab682de4
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-08 08:47:25 -0300

    ok

--- crudFuncionando.php
+++ crudFuncionando.php
@@ -100,6 +100,9 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
 <head>
     <meta charset="UTF-8">
     <title>Agenda de contatos</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
 <body>
@@ -112,7 +115,7 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
                 if (isset($id) && $id != null || $id != "") {
                     echo "value=\"{$id}\"";
                 }
-                ?> />
+                ?> class="form-control" />
         Nome:
         <input type="text" name="nome" <?php
  
@@ -120,7 +123,7 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
                if (isset($nome) && $nome != null || $nome != "") {
                    echo "value=\"{$nome}\"";
                }
-               ?> />
+               ?> class="form-control" />
         E-mail:
         <input type="text" name="email" <?php
  
@@ -128,7 +131,7 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
                if (isset($email) && $email != null || $email != "") {
                    echo "value=\"{$email}\"";
                }
-               ?> />
+               ?> class="form-control" />
         Celular:
         <input type="text" name="celular" <?php
  
@@ -136,12 +139,12 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
                if (isset($celular) && $celular != null || $celular != "") {
                    echo "value=\"{$celular}\"";
                }
-               ?> />
-        <input type="submit" value="salvar" />
-        <input type="reset" value="Novo" />
+               ?> class="form-control" />
+        <input type="submit" value="salvar" class="btn btn-success"> />
+        <input type="reset" value="Novo" class="btn btn-warning" />
         <hr>
     </form>
-    <table border="1" width="100%">
+    <table class="table table-striped table-hover">
         <tr>
             <th>Nome</th>
             <th>E-mail</th>
@@ -170,6 +173,10 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
                 }
                 ?>
     </table>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
