d812573bb088364b4849cc6436ce5890bd0bc81a
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-08 07:57:22 -0300

    ok

--- testesPDO.php
+++ testesPDO.php
@@ -14,6 +14,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $celular = NULL;
 }
 
+try {
+    $conexao = new PDO("mysql:host=localhost; dbname=crudsimples", "root", "123456");
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexao->exec("set names utf8");
+} catch (PDOException $erro) {
+    echo "Erro na conexão:" . $erro->getMessage();
+}
+
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
+    try {
+        $stmt = $conexao->prepare("INSERT INTO contatos (nome, email, celular) VALUES (?, ?, ?)");
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $email);
+        $stmt->bindParam(3, $celular);
+         
+        if ($stmt->execute()) {
+            if ($stmt->rowCount() > 0) {
+                echo "Dados cadastrados com sucesso!";
+                $id = null;
+                $nome = null;
+                $email = null;
+                $celular = null;
+            } else {
+                echo "Erro ao tentar efetivar cadastro";
+            }
+        } else {
+               throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: " . $erro->getMessage();
+    }
+}
+Anúncios
+
+
 
 ?>
 
