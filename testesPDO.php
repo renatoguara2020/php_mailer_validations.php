365f578be62c6d667a051069e743e50f243b20a1
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-08 08:22:26 -0300

    ok

--- testesPDO.php
+++ testesPDO.php
@@ -2,7 +2,7 @@
 
 // Verificar se foi enviando dados via POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
+    $id =  (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
     $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
     $email = (isset($_POST["email"]) && $_POST["email"] != null) ? $_POST["email"] : "";
     $celular = (isset($_POST["celular"]) && $_POST["celular"] != null) ? $_POST["celular"] : NULL;
@@ -56,6 +56,9 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
 <head>
     <meta charset="UTF-8">
     <title>Agenda de contatos</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
 <body>
@@ -67,32 +70,36 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
             if (isset($id) && $id != null || $id != "") {
                 echo "value=\"{$id}\"";
             }
-            ?> />
+            ?>class="form-control" />
         Nome:
         <input type="text" name="nome" <?php
             // Preenche o nome no campo nome com um valor "value"
             if (isset($nome) && $nome != null || $nome != ""){
                 echo "value=\"{$nome}\"";
             }
-            ?> />
+            ?> class="form-control" />
         E-mail:
         <input type="text" name="email" <?php
             // Preenche o email no campo email com um valor "value"
             if (isset($email) && $email != null || $email != ""){
                 echo "value=\"{$email}\"";
             }
-            ?> />
+            ?> class="form-control" />
         Celular:
         <input type="text" name="celular" <?php
             // Preenche o celular no campo celular com um valor "value"
             if (isset($celular) && $celular != null || $celular != ""){
                 echo "value=\"{$celular}\"";
             }
-            ?> />
-        <input type="submit" value="salvar" />
-        <input type="reset" value="Novo" />
+            ?> class="form-control" />
+        <input type="submit" value="salvar" class="btn btn-success" />
+        <input type="reset" value="Novo" class="btn btn-info" />
         <hr>
     </form>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
