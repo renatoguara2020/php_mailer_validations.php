d02e5642e11c7e49158c38a5025016433c63bc5a
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-09 20:07:29 -0300

    OK

---
+++ authenticationPHP.php
@@ -0,0 +1,23 @@
+<?php
+
+ // Definimos dos constantes con el nombre de usuario y su palabra clave
+ define('USUARIO', 'root');
+ define('PASSWORD', '123456');
+ 
+   // Comprobamos si existe el nombre de usuario y es correcto
+   if (!isset($_SERVER['PHP_AUTH_USER']) || 
+       ($_SERVER['PHP_AUTH_USER'] != USUARIO || $_SERVER['PHP_AUTH_PW'] != PASSWORD))
+      {
+      header('WWW-Authenticate: Basic realm=" Zona Restringida "');
+      header('HTTP/1.0 401 Unauthorized');
+      echo 'ZONA RESTRINGIDA: se requiere autorización.';
+      exit();
+      }
+
+   // Todo es correcto, le dejamos pasar...
+   else
+      {
+      echo '<h2 align="center">Zona Restringida</h2>';
+      echo 'Buenos días JEFE :)';
+      }
+?>
