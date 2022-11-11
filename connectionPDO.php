e6b86709d4cfec935d38ea4cefe85a3b9add46ea
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-11 15:30:39 -0300

    ok

--- connectionPDO.php
+++ connectionPDO.php
@@ -7,11 +7,11 @@ try{
     $conn = new PDO('mysql:host=localhost;dbname=celke', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // $stmt = $conn->prepare('INSERT INTO students (firstname, lastname, datanascimento) VALUES (:nome, :email, :message11)');
-    // $stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
-    // $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-    // $stmt->bindValue(':message1',$message, PDO::PARAM_STR);
-    // $stmt->execute();
+     $stmt = $conn->prepare('INSERT INTO students (firstname, lastname, datanascimento) VALUES (:nome, :email, :message11)');
+     $stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+     $stmt->bindValue(':message1',$message, PDO::PARAM_STR);
+     $stmt->execute();
 
     
 } catch(PDOException $e) {
