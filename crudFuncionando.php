e8636cd39fd5403db2306e9aacbf5206764db306
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-11 13:30:01 -0300

    ok

--- crudFuncionando.php
+++ crudFuncionando.php
@@ -130,6 +130,7 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/d633febe70.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
